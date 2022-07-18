dab0db24ad836a53a3f0db22905e03502faf699d
Sivabharathy <gotocva@gmail.com>
2022-07-18 15:44:55 +0530

    wallet connect issue fix

--- projects/ng-blockchain-x/src/lib/ng-blockchain-x.module.ts
+++ projects/ng-blockchain-x/src/lib/ng-blockchain-x.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
-import { ConnectWalletComponent } from './connect-wallet.component';
 import { NgBlockchainXComponent } from './ng-blockchain-x.component';
 
 
 @NgModule({
   declarations: [
     NgBlockchainXComponent,
-    ConnectWalletComponent
   ],
   imports: [
   ],
   exports: [
     NgBlockchainXComponent,
-    ConnectWalletComponent
   ]
 })
 export class NgBlockchainXModule { }
